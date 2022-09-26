364e5981666ddfaad9ee5a65639689a20aa4932a
mamphis <mattis.wacker@pcsmw.de>
2022-09-26 19:09:58 +0200

    fix: create data dir when not exists

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
+import { mkdir } from "fs/promises";
 import { Server } from "./server/server";
 
-const server = new Server(3030);
-server.start();
+// Create Data Dir
+async function start() {
+    await mkdir('./data', { recursive: true });
+
+    const server = new Server(3030);
+    server.start();
+}
+
+start();
