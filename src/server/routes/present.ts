88a0f99bd3e0c4c655f1e627df218003d16d2e4b
mamphis <mattis.wacker@pcsmw.de>
2022-09-26 23:22:41 +0200

    feature: implemented presentation mode.
    presenter can now present the slideshow.

--- src/server/routes/present.ts
+++ src/server/routes/present.ts
@@ -1,5 +1,41 @@
 import { NextFunction, Request, Response, Router } from "express";
-import { readdir, readFile } from "fs/promises";
+import { readdir, readFile, stat } from "fs/promises";
 import { resolve } from "path";
+import { MarkdownRenderer } from "../../lib/markdownrenderer";
 
 const router = Router();
+
+router.get('/:id', async (req: Request, res: Response, next: NextFunction) => {
+    return res.render('view', {
+        presentation: req.params.id
+    });
+});
+
+router.get('/:id/present', async (req: Request, res: Response, next: NextFunction) => {
+    return res.render('present', {
+        presentation: req.params.id
+    });
+});
+
+router.get('/:id/html', async (req: Request, res: Response, next: NextFunction) => {
+    const slideDir = (await readdir('./data')).find(f => f === req.params.id);
+
+    if (!slideDir) {
+        return res.redirect('/edit/' + req.params.id);
+    }
+
+    // FIXME: prevent path traversal.
+    const slidesFile = resolve('./data', slideDir, 'slides');
+    const styleFile = resolve('./data', slideDir, 'style');
+
+    const md = await stat(slidesFile).then(async () => (await readFile(slidesFile, { encoding: 'utf-8' })).toString()).catch(() => '');
+    const style = await stat(styleFile).then(async () => (await readFile(styleFile)).toString()).catch(() => '');
+
+    const html = MarkdownRenderer.it.render(md);
+    return res.json({
+        html,
+        style,
+    });
+});
+
+export default router;
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,11 +1,20 @@
 import express, { Application } from "express";
+import { createServer, Server as HttpServer } from "http";
+import { Server as SocketIoServer, Socket } from 'socket.io';
+
 import editRouter from './routes/edit';
 import themeRouter from './routes/theme';
+import presentRouter from './routes/present';
 
 export class Server {
-    app: Application;
+    private app: Application;
+    private server: HttpServer;
+    readonly io: SocketIoServer;
+
     constructor(private port: number) {
         this.app = express();
+        this.server = createServer(this.app);
+        this.io = new SocketIoServer(this.server);
 
         this.app.use(express.json());
         this.app.set('view engine', 'ejs');
@@ -15,14 +24,85 @@ export class Server {
 
         this.app.use('/theme', themeRouter);
         this.app.use('/edit', editRouter);
+        this.app.use('/present', presentRouter);
         this.app.use((req, res, next) => {
             return res.redirect('/edit');
-        })
+        });
+
+        this.configure();
     }
 
     start() {
-        this.app.listen(this.port, () => {
+        this.server.listen(this.port, () => {
             console.log(`Server started on port ${this.port}`);
+        });
+    }
+
+    private configure() {
+        const presentations: {
+            [presentation: string]: {
+                presenter?: Socket,
+                watcher: Socket[],
+            }
+        } = {};
+        this.io.on("connection", (socket) => {
+            socket.on('watch presentation', (presentation: string) => {
+                socket.join(presentation);
+                socket.to(presentation).emit('presentation started');
+
+                if (presentation! in presentations) {
+                    presentations[presentation] = {
+                        watcher: [],
+                    };
+                }
+
+                presentations[presentation].watcher.push(socket);
+            });
+
+            socket.on('present', (presentation: string) => {
+                console.log('present ' + presentation, socket.id);
+                if (presentations[presentation]?.presenter) {
+                    socket.emit('error', 'This presentation already has an presenter.');
+                    return;
+                }
+
+                socket.join(presentation);
+                socket.to(presentation).emit('presentation started');
+
+                if (!(presentation in presentations)) {
+                    presentations[presentation] = {
+                        watcher: [],
+                    };
+                }
+
+                presentations[presentation].presenter = socket;
+            });
+
+            socket.on('set slide', (presentation: string, slide: number) => {
+                console.log('setting slide', socket.id, presentation, slide);
+                if (presentations[presentation]?.presenter?.id !== socket.id) {
+                    socket.emit('error', 'You are not the presenter of this presentation.');
+                    return;
+                }
+
+                socket.to(presentation).emit('set slide', slide);
+            });
+
+            socket.on('disconnecting', () => {
+                console.log("socket disconnecting", socket.id);
+
+                socket.rooms.forEach(room => {
+                    console.log("leaving rooms", room);
+                    if (!presentations[room]) {
+                        return;
+                    }
+
+                    presentations[room].watcher = presentations[room].watcher.filter(w => w.id !== socket.id);
+                    if (presentations[room]?.presenter?.id === socket.id) {
+                        presentations[room].presenter = undefined;
+                    }
+                });
+            });
         });
     }
 }
